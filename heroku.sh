9c6262d5b8d3b1603e7c4819de9cbb1ba96a52ec
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2017-08-03 02:10:00 +0000

    working heroku script

--- heroku.sh
+++ heroku.sh
@@ -5,10 +5,11 @@ heroku create kekeno-stage
 # heroku buildpacks:set https://github.com/jordansissel/heroku-buildpack-meteor.git -a kekeno-stage
 heroku buildpacks:set https://github.com/AdmitHub/meteor-buildpack-horse.git
 
+heroku addons:create mongolab -a kekeno-stage
 # heroku addons:create mongolab:shared-cluster-1 -a kekeno-stage
 # heroku addons:create mongohq:ssd_1g_elastic -a kekeno-stage
 
-heroku config:set ROOT_URL=http://www.kekenopress.co.nz/ -a kekeno-stage
+heroku config:set ROOT_URL=https://kekeno-stage.herokuapp.com/ -a kekeno-stage
 
 # heroku config:add METEOR_SETTINGS="$(cat settings.json)" -a kekeno-stage
 
