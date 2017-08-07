a2e6f55146102aa160a395f77707f0bbb24cc13a
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2017-08-07 23:04:57 +1200

    updates

--- heroku.sh
+++ heroku.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-heroku create kekeno-stage
+read -p "Dyno name: "
 
-# heroku buildpacks:set https://github.com/jordansissel/heroku-buildpack-meteor.git -a kekeno-stage
+heroku create $REPLY
+
+# heroku buildpacks:set https://github.com/jordansissel/heroku-buildpack-meteor.git -a $REPLY
 heroku buildpacks:set https://github.com/AdmitHub/meteor-buildpack-horse.git
 
-heroku addons:create mongolab -a kekeno-stage
-# heroku addons:create mongolab:shared-cluster-1 -a kekeno-stage
-# heroku addons:create mongohq:ssd_1g_elastic -a kekeno-stage
+heroku addons:create mongolab -a $REPLY
+# heroku addons:create mongolab:shared-cluster-1 -a $REPLY
+# heroku addons:create mongohq:ssd_1g_elastic -a $REPLY
 
-heroku config:set ROOT_URL=https://kekeno-stage.herokuapp.com/ -a kekeno-stage
+heroku config:set ROOT_URL=https://$REPLY.herokuapp.com/ -a $REPLY
 
-# heroku config:add METEOR_SETTINGS="$(cat settings.json)" -a kekeno-stage
+# heroku config:add METEOR_SETTINGS="$(cat settings.json)" -a $REPLY
 
 git push heroku master
